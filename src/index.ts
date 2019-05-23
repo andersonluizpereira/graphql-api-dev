d937bdfe1963306464eceed21b41dd91348b2d28
Anderson <andersononus@hotmail.com>
2019-05-23 19:50:52 -0300

    implementando onserver listening and onerror

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import * as http from 'http'
 import app from './app'
+import { normalizePort, onError, onListening } from './utils/utils';
 
 const server = http.createServer(app);
-server.listen(3000);
-server.on('listening', ()=> console.log('Listening port 3000'));
+const port = normalizePort(process.env.PORT || 3000)
+server.listen(port);
+server.on('error', onError(server))
+server.on('listening', onListening(server));
