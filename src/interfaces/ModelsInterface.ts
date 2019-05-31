17e1f62455a4d1d841ad886201602c4b24c0efa7
Anderson <andersononus@hotmail.com>
2019-05-31 16:33:48 -0300

    criando modelo de postmodel

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,3 +1,7 @@
+import { PostModel } from "../model/PostModel";
+import { UserModel } from "../model/UserModel";
+
 export interface ModelsInterface {
-    
+    Post: PostModel;
+    User: UserModel;
 }
