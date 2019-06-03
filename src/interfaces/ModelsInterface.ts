793421f2e0e35df6137bb2b9cd98442014d61873
Anderson <andersononus@hotmail.com>
2019-06-03 17:34:39 -0300

    CommentModel

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,7 +1,9 @@
 import { PostModel } from "../model/PostModel";
 import { UserModel } from "../model/UserModel";
+import { CommentModel } from "../model/CommentModel";
 
 export interface ModelsInterface {
     Post: PostModel;
     User: UserModel;
+    Comment: CommentModel;
 }
