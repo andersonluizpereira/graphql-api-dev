f000cfd3ddd7c055ebf5099072bc1e0f4cf6ed16
Anderson <andersononus@hotmail.com>
2019-06-13 11:25:05 -0300

    reolvers mutation

--- src/graphql/resources/user/user.resolvers.ts
+++ src/graphql/resources/user/user.resolvers.ts
@@ -1,6 +1,7 @@
 import { GraphQLResolveInfo } from "graphql";
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { UserInstance } from "../../../model/UserModel";
+import { Transaction } from "sequelize";
 
 export const userResolvers = {
     Query: {
@@ -20,5 +21,44 @@ export const userResolvers = {
                     });
         }
 
+    },
+    Mutation: {
+        createUser: (parent, {input}, {db}: {db:DbConnection}, info: GraphQLResolveInfo) => { 
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.User.create(input, {transaction :t});
+            });
+        },
+        updateUser: (parent, {id, input}, {db}: {db:DbConnection}, info: GraphQLResolveInfo) => { 
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.User
+                .findById(id).then((user: UserInstance) => {
+                    if(!user) throw new Error(`User with id ${id} not found!`);
+                    user.update(input, {transaction :t});
+                });
+            });
+        },
+        updateUserPassword: (parent, {id, input}, {db}: {db:DbConnection}, info: GraphQLResolveInfo) => { 
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.User
+                .findById(id).then((user: UserInstance) => {
+                    if(!user) throw new Error(`User with id ${id} not found!`);
+                    user.update(input, {transaction :t}).then((user: UserInstance) => !! user);
+                });
+            });
+        },
+        deleteUser: (parent, {id}, {db}: {db:DbConnection}, info: GraphQLResolveInfo) => { 
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.User
+                    .findById(id)
+                    .then((user: UserInstance) => {
+                    if(!user) throw new Error(`User with id ${id} not found!`);
+                        return user.destroy({transaction: t})
+                            .then(user => !!user);
+                    });
+            });
+        }
     }
 } 
