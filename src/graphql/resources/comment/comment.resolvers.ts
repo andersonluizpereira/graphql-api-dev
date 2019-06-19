54d71e751fc6df921ab1bec4c9bab69c19dc05a3
Anderson <andersononus@hotmail.com>
2019-06-19 16:52:15 -0300

    Comment resolvers: Mutations

--- src/graphql/resources/comment/comment.resolvers.ts
+++ src/graphql/resources/comment/comment.resolvers.ts
@@ -1,5 +1,7 @@
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { GraphQLResolveInfo } from "graphql";
+import { Transaction } from "sequelize";
+import { CommentInstance } from "../../../model/CommentModel";
 
 
 export const commentResolvers = {
@@ -19,6 +21,37 @@ export const commentResolvers = {
                 where: {post: postId},
                 limit: first,
                 offset: offset
+            });
+        }
+    },
+    Mutation: {
+        createComment: (parent, {input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .create(input, {transaction: t});
+            });
+        },
+        updateComment: (parent, {id, input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id  = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                .findById(id)
+                .then((commentt: CommentInstance) =>{
+                    if(!commentt) throw new Error(`Comment with id ${id} not found!`);
+                    return commentt.update(input, {transaction: t});
+                });
+            });
+        },
+        deleteComment: (parent, {id, input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id  = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                .findById(id)
+                .then((comment: CommentInstance) =>{
+                    if(!comment) throw new Error(`Comment with id ${id} not found!`);
+                    return comment.destroy({transaction: t})
+                        .then(comment => !!comment);
+                });
             });
         }
     }
