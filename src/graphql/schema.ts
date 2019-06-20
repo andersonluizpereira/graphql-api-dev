96e7792c07c0bcd06970caf08547c9c28c113e65
Anderson <andersononus@hotmail.com>
2019-06-20 10:54:18 -0300

    Adicionando resolvers para execução no schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -5,6 +5,17 @@ import { userTypes } from './resources/user/user.schema';
 import { postTypes } from './resources/post/post.schema';
 import { commentTypes } from './resources/comment/comment.schema';
 
+import { merge } from 'lodash';
+import { commentResolvers } from './resources/comment/comment.resolvers';
+import { postResolvers } from './resources/post/post.resolvers';
+import { userResolvers } from './resources/user/user.resolvers';
+
+const resolvers = merge(
+    commentResolvers,
+    postResolvers,
+    userResolvers
+);
+
 const SchemaDefinition = `
     type Schema {
         query: Query
@@ -21,5 +32,6 @@ export default makeExecutableSchema({
         postTypes,
         commentTypes
         
-    ]
+    ],
+    resolvers
 })
