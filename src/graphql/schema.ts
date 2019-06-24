0ffe3f6c1faf2f826a1a9eb22bc49ce1771553f7
Anderson <andersononus@hotmail.com>
2019-06-24 14:21:26 -0300

    Token resolver: createToken e JWT_SECRET

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -10,10 +10,12 @@ import { commentResolvers } from './resources/comment/comment.resolvers';
 import { postResolvers } from './resources/post/post.resolvers';
 import { userResolvers } from './resources/user/user.resolvers';
 import { tokenTypes } from './resources/token/token.schema';
+import { tokenResolvers } from './resources/token/token.resolvers';
 
 const resolvers = merge(
     commentResolvers,
     postResolvers,
+    tokenResolvers,
     userResolvers
 );
 
