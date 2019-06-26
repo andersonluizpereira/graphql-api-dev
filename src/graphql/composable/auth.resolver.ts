5adaa4ba4708d6b3ee0120c0aee30f32764659b2
Anderson <andersononus@hotmail.com>
2019-06-25 22:06:36 -0300

    evitando colisao

--- src/graphql/composable/auth.resolver.ts
+++ src/graphql/composable/auth.resolver.ts
@@ -5,7 +5,7 @@ import { GraphQLFieldResolver } from "graphql";
 export const authResolver: ComposableResolver<any, ResolverContext> = 
     (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
         return (parent, args, context: ResolverContext, info) => {
-            if(context.user || context.authorization) {
+            if(context.authUser || context.authorization) {
                 return resolver(parent, args, context, info);
             }
             throw new Error('UnAuthorized! Token not provided!');
--- src/interfaces/ResolverContextInterface.ts
+++ src/interfaces/ResolverContextInterface.ts
@@ -2,8 +2,7 @@ import { DbConnection } from "./DbConnectionInterface";
 import { AuthUser } from "./AuthUserInterface";
 
 export interface ResolverContext {
-    authUser: string;
     db?: DbConnection;
     authorization?: string;
-    user?: AuthUser;
+    authUser?: AuthUser;
 }
