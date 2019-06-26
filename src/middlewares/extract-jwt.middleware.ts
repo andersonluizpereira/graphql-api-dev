8c1f723c693ff83633a138f0a4aa3fabbdf0c576
Anderson <andersononus@hotmail.com>
2019-06-25 21:28:08 -0300

    resolvido

--- src/middlewares/extract-jwt.middleware.ts
+++ src/middlewares/extract-jwt.middleware.ts
@@ -1,32 +1,39 @@
+import * as jwt from 'jsonwebtoken';
+ 
+import db from './../model';
 import { RequestHandler, Request, Response, NextFunction } from "express";
-import * as jwt from 'jsonwebtoken'
-import { JWT_SECRET } from "../utils/utils";
-import db from './../model'
-import { UserInstance } from "../model/UserModel";
-
-
+import { JWT_SECRET } from '../utils/utils';
+import { UserInstance } from '../model/UserModel';
+ 
 export const extractJwtMiddleware = (): RequestHandler => {
+ 
     return (req: Request, res: Response, next: NextFunction): void => {
-        let authorization: string = req.get('autorization');
-        let token: string  = authorization ? authorization.split(' ')[1] : undefined;
-
+ 
+        let authorization: string = req.get('authorization');
+        let token: string = authorization ? authorization.split(' ')[1] : undefined;
+ 
         req['context'] = {};
         req['context']['authorization'] = authorization;
-
-        if(!token) { return next(); }
-
-        jwt.verify(token, JWT_SECRET, (err, decode: any) =>{
-            if(err) { return next(); }
-            db.User.findById(decode.sub, {
-                attributes: ['id','email']
+ 
+        if (!token) { return next(); }
+ 
+        jwt.verify(token, JWT_SECRET, (err, decoded: any) => {
+ 
+            if (err) { return next(); }
+ 
+            db.User.findById(decoded.sub, {
+                attributes: ['id', 'email']
             }).then((user: UserInstance) => {
-                if(user) {
+ 
+                if (user) {
                     req['context']['user'] = {
                         id: user.get('id'),
                         email: user.get('email')
-                    }
+                    };
                 }
+ 
                 return next();
+                
             });
         });
     };
