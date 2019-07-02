9bbfb829d327a742d481f4c5087a7e70e64872fa
Anderson <andersononus@hotmail.com>
2019-07-02 07:15:06 -0300

    criando dataloader param

--- src/graphql/dataloaders/DataLoaderFactory.ts
+++ src/graphql/dataloaders/DataLoaderFactory.ts
@@ -5,18 +5,21 @@ import { UserLoader } from "./UserLoader";
 import { UserInstance } from '../../model/UserModel';
 import { PostInstance } from '../../model/PostModel';
 import { PostLoader } from './PostLoader';
+import { RequestedFields } from '../ast/RequestedFields';
+import { DataLoaderParam } from '../../interfaces/DataLoaderInterface';
 
 export class DataLoaderFactory {
     constructor(
-        private db: DbConnection
+        private db: DbConnection,
+        private requestdFields: RequestedFields
         ) {}
     getLoaders(): DataLoaders {
         return {
-            userLoader: new DataLoader<number, UserInstance> (
-                (ids: number[]) => UserLoader.batchUsers(this.db.User, ids)
+            userLoader: new DataLoader<DataLoaderParam<number>, UserInstance> (
+                (params: DataLoaderParam<number>[]) => UserLoader.batchUsers(this.db.User, params)
             ),
-            postLoader: new DataLoader<number, PostInstance> (
-                (ids: number[]) => PostLoader.batchPosts(this.db.Post, ids)
+            postLoader:  new DataLoader<DataLoaderParam<number>, PostInstance> (
+                (params: DataLoaderParam<number>[]) => PostLoader.batchPosts(this.db.Post, params)
             )
         }
     }    
--- src/interfaces/DataLoadersInterface.ts
+++ src/interfaces/DataLoadersInterface.ts
@@ -1,8 +1,9 @@
 import * as DataLoader from 'dataloader'
 import { UserInstance } from '../model/UserModel';
 import { PostInstance } from '../model/PostModel';
+import { DataLoaderParam } from './DataLoaderInterface';
 
 export interface DataLoaders {
-    userLoader: DataLoader<number, UserInstance>;
-    postLoader: DataLoader<number, PostInstance>;
+    userLoader: DataLoader<DataLoaderParam<number>, UserInstance>;
+    postLoader: DataLoader<DataLoaderParam<number>, PostInstance>;
 }
