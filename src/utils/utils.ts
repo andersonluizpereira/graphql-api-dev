8ec2202736a0989ab456f13f8730594145082660
Anderson <andersononus@hotmail.com>
2019-07-18 16:41:05 -0300

    ajustando testes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,15 @@
-import { Server } from "http";
-
-export const normalizePort = (val: number | string): number | string | boolean => {
-    let port: number = (typeof val === 'string') ? parseInt(val) : val;
-    if (isNaN(port)) return val;
-    else if (port >= 0) return port;
-    else return false;
-}
-
-export const onError = (server: Server) => {
-    return (error: NodeJS.ErrnoException): void => {
-        let port: number | string = server.address().port;
-        if (error.syscall !== 'listen') throw error;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.normalizePort = (val) => {
+    return (typeof val === 'string') ? parseInt(val) : val;
+};
+exports.onError = (server) => {
+    return (error) => {
+        let port = server.address().port;
+        if (error.syscall !== 'listen')
+            throw error;
         let bind = (typeof port === 'string') ? `pipe ${port}` : `port ${port}`;
-        switch(error.code) {
+        switch (error.code) {
             case 'EACCES':
                 console.error(`${bind} requires elevated privileges`);
                 process.exit(1);
@@ -24,26 +21,26 @@ export const onError = (server: Server) => {
             default:
                 throw error;
         }
-    }
-}
-
-export const onListening = (server: Server) => {
-    return (): void => {
-        let addr = server.address();
-        let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-        console.log(`Listening at ${bind}...`);
-    }
-}
-
-export const handleError = (error: Error) => {
-    let errorMessage: string = `${error.name}: ${error.message}`;
-    console.log(errorMessage);
-    return Promise.reject(new Error(errorMessage));
-}
-
-export const throwError = (condition: Boolean, message: string): void => {
-    if(condition) { throw new Error(message); };
+    };
 };
-
-
-export const JWT_SECRET: string = process.env.JWT_SECRET;
+exports.onListening = (server) => {
+    return () => {
+        let addr = server.address();
+        let bind = (typeof addr === 'string') ? `pipe ${addr}` : `http://${addr.address}:${addr.port}`;
+        console.log(`Listening at ${bind}...`);
+    };
+};
+exports.handleError = (error) => {
+    let errorMessage = `${error.name}: ${error.message}`;
+    let env = process.env.NODE_ENV;
+    if (env !== 'test' && env !== 'pipelines') {
+        console.log(errorMessage);
+    }
+    return Promise.reject(new Error(errorMessage));
+};
+exports.throwError = (condition, message) => {
+    if (condition) {
+        throw new Error(message);
+    }
+};
+exports.JWT_SECRET = process.env.JWT_SECRET;
